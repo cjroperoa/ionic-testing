5ebe5e603e01dd62cc39023f93d005079eb85dc4
cjropero <cjavieramaya@gmail.com>
2019-03-08 21:18:37 -0500

    creo getArea()

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  base;
+  altura;
+  rest;
+  
+  getArea(){
+    return this.rest = `El Area de su rectangulo es: ${this.base*this.altura}`;
+   }
+}
