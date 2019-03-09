2f4adefdbb0a19c3b5b54c4c6397d887f429ad79
cjropero <cjavieramaya@gmail.com>
2019-03-08 21:43:39 -0500

    funciones de la vista perimetro

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  base;
+  altura;
+  rest;
+  
+  getPerimeter(){
+    return this.rest = `El perimetro de su rectangulo es: ${(this.base*2)+(this.altura*2)}`;
+   }
+}
